018ba2ff17bc21fb7a0ca40c889d50f8bc84ede6
Stanislav Sukharev <Stanislav_Sukharev@epam.com>
2022-09-21 22:05:36 +0400

    module2 v1.0

--- tasks/module-2/variables.js
+++ tasks/module-2/variables.js
@@ -6,35 +6,40 @@
 /**
  * create variable named int and set value equal to 1
  */
-
+let int = 1;
 /**
  *  create variable named int2 and set value equal int plus 1
  */
-
+let int2 = int + 1;
 /**
  * create variable named str and set value equal to "John"
  */
-
+let str = 'John';
 /**
  * create variable named str2 and set value equal str plus " Dou"
  */
+let str2 = str + ' Dou';
 
 /**
  * create variable named bool and set value of equality comparison of int and int2 variables
  */
-
+let bool = int == int2;
 /**
  * create variable named arr and set value array of numbers from 1 to 5
  */
-
+const arr = [1, 2, 3, 4, 5];
 /**
  * create variable named fifth and set value as element of array with index 4
  */
-
+let fifth = arr[4];
 /**
  * create variable named human and set value as object with key firstName and value "John"
  */
-
+const human = {
+  firstName: 'John',
+  lastName: 'Dou',
+  fullName: 'John' + ' ' + 'Dou',
+};
 /**
  * set new key of human object named lastName and value "Dou"
  */
