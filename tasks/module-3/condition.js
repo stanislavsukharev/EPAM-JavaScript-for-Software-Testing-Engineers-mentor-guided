f05e02a65eafc0bbe739dca31b8cbd19dfab0a39
Stanislav Sukharev <Stanislav_Sukharev@epam.com>
2022-09-23 00:34:50 +0400

    hometask3 v1.0.0

--- tasks/module-3/condition.js
+++ tasks/module-3/condition.js
@@ -9,7 +9,11 @@
 function taskA(x, y) {
   let z1;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+  if (y < 10) {
+    z1 = x + y;
+  } else {
+    z1 = x * y;
+  }
   //AND THIS ONE:
   return z1;
 }
@@ -17,7 +21,7 @@ function taskA(x, y) {
 function taskB(x, y) {
   let z2;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+  z2 = y < 10 ? x + y : x * y;
   //AND THIS ONE:
   return z2;
 }
@@ -33,7 +37,20 @@ function taskB(x, y) {
 function calc(operator, alpha, beta) {
   let task2;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+  switch (operator) {
+    case 'add':
+      task2 = alpha + beta;
+      break;
+    case 'subtract':
+      task2 = alpha - beta;
+      break;
+    case 'multiply':
+      task2 = alpha * beta;
+      break;
+    case 'divide':
+      task2 = alpha / beta;
+      break;
+  }
   //AND THIS ONE:
   return task2;
 }
@@ -53,7 +70,15 @@ function calc(operator, alpha, beta) {
 function calculateDiscount(ransom) {
   let discount;
   //PLACE YOUR CODE BETWEEN THIS LINE:
-
+  if (ransom > 0 && ransom <= 350) {
+    discount = 0;
+  } else if (ransom >= 351 && ransom <= 1350) {
+    discount = 15;
+  } else if (ransom >= 1351 && ransom <= 2700) {
+    discount = 30;
+  } else if (ransom >= 2701 && ransom <= 6500) {
+    discount = 45;
+  }
   //AND THIS ONE:
   return discount;
 }
--- tasks/module-3/cycles.js
+++ tasks/module-3/cycles.js
@@ -7,15 +7,36 @@
  */
 let forFactorial, whileFactorial, doFactorial;
 const n = 10; // 10! = 3628800
+forFactorial = 1;
+whileFactorial = 1;
+doFactorial = 1;
 
+for (let i = 1; i <= n; i++) {
+  forFactorial *= i;
+}
+
+let i = 1;
+while (i <= n) {
+  whileFactorial *= i;
+  i++;
+}
+
+let j = 1;
+do {
+  doFactorial *= j;
+  j++;
+} while (j <= n);
 /** Task: 2
  * return the concatenated string from an array of substring
  * assign the result to variable 'str'
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
  */
 let str;
+str = '';
 const substr = ['I', ' love', ' JS'];
-
+for (let el of substr) {
+  str += el;
+}
 /** Task: 3
  * calculate a total of income of certain person
  * assign the result to the variable 'totalIncome'
@@ -28,6 +49,11 @@ const personIncomes = {
   interestOnDeposit: 250,
   otherExpences: -300,
 };
+
+totalIncome = 0;
+for (let i in personIncomes) {
+  totalIncome += personIncomes[i];
+}
 
 module.exports = {
   forFactorial,
